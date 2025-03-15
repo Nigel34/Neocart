3f1e8eb30bef03a875877dbe2f0fb115947f2bb6
Nigel34 <ichenihinigel@gmail.com>
2025-03-15 22:28:15 +0300

    freebird 1

--- NeoApp/models.py
+++ NeoApp/models.py
@@ -1,24 +1,17 @@
 from urllib import request
 
 from django.db import models
-from django.shortcuts import render
 
 
-# Create your models here.
-def index(request):
-    return render(request, 'index.html')
 
-def blog(request):
-    return render(request, 'blog.html')
 
-def starter(request):
-    return render(request,'starter-page.html')
+class Transaction(models.Model):
+    phone_number = models.CharField(max_length=15)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_id = models.CharField(max_length=100, unique=True)
+    status = models.CharField(max_length=20, choices=[('Success', 'Success'), ('Failed', 'Failed')])
+    date = models.DateTimeField(auto_now_add=True)
 
-def portfolio(request):
-     return render(request, 'portfolio-details.html')
+    def __str__(self):
+        return f"{self.phone_number} - {self.amount} - {self.status}"
 
-def blog_details(request):
-    return render(request, 'blog-details.html')
-
-def service(request):
-    return render(request, 'service-details.html')
--- NeoApp/urls.py
+++ NeoApp/urls.py
@@ -6,11 +6,16 @@ from NeoApp import views
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
     path('blog/',views.blog,name='blog'),
     path('blogs/',views.blog_details,name='blogs'),
     path('starter/',views.starter,name='starter'),
     path('portfolio/',views.portfolio,name='portfolio'),
-
+    path('transaction/', views.transactions_list, name='transaction'),
+    path('', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('pay/', views.pay, name='pay'),
+    path('stk/', views.stk, name='stk'),
+    path('token/', views.token, name='token'),
 
 ]
